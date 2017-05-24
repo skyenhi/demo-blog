75a76379d984b030d8bb8a76dd49edb4cd935ef9
skyenhi <skye.nhi@1pac.vn>
2017-05-24 17:38:13 +0700

    Post detail

---
+++ frontend/src/pages/Article.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="article">
+        <div id="content">
+            <h1>{{ article.title }}</h1>
+            <p>{{ article.body }}</p>
+        </div>
+        <comment></comment>
+
+    </div>
+</template>
+
+<script>
+    import {mapActions, mapState}   from 'vuex'
+    import Comment  from '../components/Comment.vue'
+
+
+    export default {
+        components: {
+            Comment
+        },
+        created () {
+            this.getArticle(this.$route.params.id)
+        },
+        computed: {
+            ...mapState(['article'])
+        },
+        methods: {
+            ...mapActions(['getArticle'])
+        }
+    }
+</script>
+
+<style scoped></style>
