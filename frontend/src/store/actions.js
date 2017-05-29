ec3cbc8dcdc7f5508e55354e3817a89ac72fe1e4
skyenhi <skye.nhi@1pac.vn>
2017-05-29 17:07:35 +0700

    Optimise fun

--- frontend/src/store/actions.js
+++ frontend/src/store/actions.js
@@ -4,10 +4,10 @@ import { API_ROOT } from '../config'
 export default {
   getArticleList ({commit}) {
   return Vue.http.get(API_ROOT + '/posts')
-     .then(response => response.json())
-  .then(articles => {
-    commit('set_all_articles', articles)
-    }).catch((err) => { console.log(err) })
+    .then(response => response.json())
+      .then(articles => {
+        commit('set_all_articles', articles)
+      }).catch((err) => { console.log(err) })
   },
 
   getArticle ({commit}, aid) {
@@ -19,15 +19,15 @@ export default {
 
   getAllComments ({commit}, aid) {
     return Vue.http.get(API_ROOT + '/comments?postId=' + aid)
-            .then(response => response.json())
-  .then(comments => {
+      .then(response => response.json())
+      .then(comments => {
       commit('set_comments', comments)
     }).catch((err) => { console.log(err) })
   },
 
   getUser ({commit}, aid) {
     return Vue.http.get(API_ROOT + '/users/' + aid)
-            .then(response => {
+       .then(response => {
       commit('set_user', response.data)
     }).catch((err) => { console.log(err) })
   }
