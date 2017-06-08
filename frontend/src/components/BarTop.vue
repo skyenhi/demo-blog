2e85fe31070273ad1a28b1e9a46d7b3610c24cf9
skyenhi <skye.nhi@1pac.vn>
2017-06-08 16:24:10 +0700

    Code style format

--- frontend/src/components/BarTop.vue
+++ frontend/src/components/BarTop.vue
@@ -1,49 +1,39 @@
 <template>
-    <header>
-        <div id="global-navigation" class="container">
-            <div class="header-logo text-center"><router-link to="/"><img src="/app/img/logo.png" alt="logo"/></router-link></div>
-            <div class="sidebar-menu">
-                <a @click="showAside = !showAside">
-                    <span class="push-overlay" v-show="showAside"></span>
-                    <span id="global-menu-btn" v-show="!showAside"></span>
-                </a>
-            </div>
-            <transition name="slide-fade">
-                <nav  v-if="showAside">
-                    <ul>
-                        <li @click="showAside = !showAside"><router-link :to="'/'">Home</router-link></li>
-                        <li @click="showAside = !showAside"><router-link :to="'/group-training'">Group Training</router-link></li>
-                        <li @click="showAside = !showAside"><router-link :to="'/articles'">Blog</router-link></li>
-                        <li @click="showAside = !showAside"><router-link :to="'/about'">About Bee</router-link></li>
-                        <li @click="showAside = !showAside"><router-link :to="'/contact'">Contact</router-link></li>
-                    </ul>
-                </nav>
-            </transition>
-            <div class="col global-menu">
-                <ul class="list-inline menu">
-                    <li><router-link :to="'/'">Home</router-link></li>
-                    <li><router-link :to="'/group-training'">Group Training</router-link></li>
-                    <li><router-link :to="'/articles'">Blog</router-link></li>
-                    <li><router-link :to="'/about'">About Bee</router-link></li>
-                    <li><router-link :to="'/contact'">Contact</router-link></li>
-                </ul>
-            </div>
-        </div>
-    </header>
+  <header>
+    <div id="global-navigation" class="container">
+      <div class="header-logo text-center"><router-link to="/"><img src="/app/img/logo.png" alt="logo"/></router-link></div>
+      <div class="sidebar-menu">
+        <a @click="showAside = !showAside">
+          <span class="push-overlay" v-show="showAside"></span>
+          <span id="global-menu-btn" v-show="!showAside"></span>
+        </a>
+      </div>
+      <transition name="slide-fade">
+        <nav  v-if="showAside">
+          <ul>
+            <li @click="showAside = !showAside"><router-link :to="'/'">Home</router-link></li>
+            <li @click="showAside = !showAside"><router-link :to="'/group-training'">Group Training</router-link></li>
+            <li @click="showAside = !showAside"><router-link :to="'/articles'">Blog</router-link></li>
+            <li @click="showAside = !showAside"><router-link :to="'/about'">About Bee</router-link></li>
+            <li @click="showAside = !showAside"><router-link :to="'/contact'">Contact</router-link></li>
+          </ul>
+        </nav>
+      </transition>
+    </div>
+  </header>
 </template>
 
-
 <script>
-    export default {
-        data() {
-            return {
-                showAside: false,
-            }
-        },
-        methods: {
-            goBack() {
-                this.$router.back();
-            }
-        }
-    };
+  export default {
+    data() {
+      return {
+        showAside: false,
+      }
+    },
+    methods: {
+      goBack() {
+        this.$router.back();
+      }
+    }
+  };
 </script>
